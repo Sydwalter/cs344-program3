e47874bf9a2be661f2fc6343f54e85cf94258313
Scott Milton <miltons@oregonstate.edu>
2015-11-25 08:21:29 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Program 3 for CS 344 at Oregon State University.
---
+++ program3/smallsh.c
@@ -0,0 +1,43 @@
+/*****************************************************************************
+ ** Filename:    smallsh.c
+ ** Author:      Scott Milton
+ ** Date:        11/23/15
+ **
+ ** Description: This project 
+ **
+ ** Input:       from the keyboard: type char*
+ **              from files:        type char*
+ **
+ ** Output:      to the console and files : type char*, int
+ **              to files:                  type char*, int
+ **
+ **
+ **
+ *****************************************************************************/
+
+/*
+#include <errno.h>     // for errno
+#include <unistd.h>    // for getpid
+#include <sys/types.h> // for pid_t
+#include <stdio.h>     // for fgets, fopen, fclose, fseek
+#include <stdlib.h>    // for rand and srand
+#include <sys/stat.h>  // for stat
+#include <string.h>    // for strcpy, strcat
+#include <time.h>      // for time
+#include <fcntl.h>     // for open
+*/
+
+/*
+#define BUFFER_SIZE     512 // used for I/O operations
+*/
+
+// define bool as type
+typedef enum { false, true } bool;
+
+
+
+int main(int argc, char** argv) {
+
+
+    return 0;
+}
